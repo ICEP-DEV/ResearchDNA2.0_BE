06f311321fbaabacf5f08eedf29f681f2d073e14
mashabelaherold <95069470+mashabelaherold@users.noreply.github.com>
2022-08-31 11:38:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+//1 REQUIRE PACKAGES
+const express = require('express');
+const morgan = require('morgan');
+const globalErrHandler = require('./utils/errHandler.js');
+const cors = require('cors');
+
+const app = express();
+
+//2 REQUIRE ROUTES (WHICH WE DO NOT HAVE YET)
+
+const signupRoute = require('./routes/signupRoute');
+
+
+
+//3 APP.USE MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
+
+//4 CREATE API URL
+
+app.use('/api/signup', signupRoute);
+
+
+
+
+app.use(globalErrHandler);
+module.exports = app;
