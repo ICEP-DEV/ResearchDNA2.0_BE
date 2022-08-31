dc1936a7217ce05f1aca1609167952ca8c819c1d
mashabelaherold <95069470+mashabelaherold@users.noreply.github.com>
2022-08-31 11:40:19 +0200

    Add files via upload

---
+++ routes/signupRoute.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const signupController = require('../controller/signupController');
+
+router
+    .route("/")
+    .post(signupController.createSignup)
+
+module.exports = router;
