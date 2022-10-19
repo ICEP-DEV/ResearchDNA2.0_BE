4830a2d80a6ef821d0727d2dc97a74573087cb5a
mashabelaherold <95069470+mashabelaherold@users.noreply.github.com>
2022-10-19 10:25:56 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,43 +3,26 @@ const express = require('express');
 const morgan = require('morgan');
 const globalErrHandler = require('./utils/errHandler.js');
 const cors = require('cors');
-const bodyparser = require('body-parser');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
 //2 REQUIRE ROUTES (WHICH WE DO NOT HAVE YET)
-
-const signupRoute = require('./routes/signupRoute');
-const loginRoute = require('./routes/loginRoute');
-
-
+const supervisorRoute = require('./routes/supervisorRoute');
+const announcementRoute = require('./routes/announcementRoute'); 
+const feedbackRoute = require('./routes/feedbackRoute'); 
+const userRoute = require('./routes/userRoute');                      
 
 //3 APP.USE MIDDLEWARE
 app.use(express.json());
 app.use(cors());
 app.options('*', cors());
-app.use|(cookieParser());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(bodyparser.json());
-
-const oneDay = 1000*60*60*24;
-
-app.use(session({
-    secret:'thisismysecret_keysadhasdkjasdh',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: oneDay }
-}));
 
 
 //4 CREATE API URL
-
-app.use('/api/signup', signupRoute);
-app.use('/api/login', loginRoute);
-
-
+app.use('/api/supervisor', supervisorRoute);
+app.use('/api/announcement', announcementRoute);
+app.use('/api/feedback', feedbackRoute);
+app.use('/api/user', userRoute);
 
 
 app.use(globalErrHandler);
